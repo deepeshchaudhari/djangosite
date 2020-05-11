62a267e9c8a156d7d0786c958b055b1bd66adf96
Deepesh Chaudhari <deepeshc@cse.iitk.ac.in>
2020-05-11 22:06:06 +0530

    "punctation remover and chsngr to uppercase done"

--- mysite/urls.py
+++ mysite/urls.py
@@ -16,10 +16,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from . import views
-
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',views.index,name='index'),
     path('about',views.about,name='about'),
-    path('readtext',views.readtext,name='readtext')
+    path('readtext',views.readtext,name='readtext'),
+    path('analyze',views.analyze,name='analyze')
 ]
+
--- mysite/views.py
+++ mysite/views.py
@@ -5,10 +5,45 @@
 from django.http import HttpResponse
 from  django.shortcuts import render
 def  index(request):
-    params = {'name': 'Deepesh Chaudhari', 'place': 'Kanpur'}
-    return render(request,'index.html',params)
+    # params = {'name': 'Deepesh Chaudhari', 'place': 'Kanpur'}
+    return render(request,'index.html')
+
 def  about(request):
     return render(request,'about.html')
+
+def  analyze(request):
+    # get and analyze the text
+    djtext = request.GET.get('text','default')
+    # checknox value
+    removepunc = request.GET.get('removepunc','off')
+    fullcaps = request.GET.get('fullcaps','off')
+
+    punctuation = '''!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~0123456789'''
+    analyzed =''
+    purpose =''
+    # apply operation according to checkbos value
+    if removepunc =='on' and fullcaps=='off':
+        purpose ="Punctuation Removal"
+        for char in djtext:
+            if char not in punctuation:
+                analyzed+=char
+        print(analyzed)
+    elif removepunc =='on' and fullcaps=='on':
+        purpose ="Punctuation Removal and change to uppercase"
+        for char in djtext:
+            if char not in punctuation:
+                analyzed+=char
+        analyzed = analyzed.upper()
+    elif removepunc =='off' and fullcaps=='on':
+        purpose ="Change to uppercase"
+        analyzed = djtext.upper()
+    else:
+        analyzed=djtext
+
+    # set to variable and send to html page
+    param ={'purpose': purpose ,'analyzing': analyzed}
+    return render(request,'analyze.html',param)
+
 def  readtext(request):
     with open('E:/IIT KANPUR CSE/djangotuts/mysite/mysite/commands.txt') as f:
         data = f.read()
