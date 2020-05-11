76da81795aa142b1c42128ea57fae93fccce86d7
Deepesh Chaudhari <deepeshc@cse.iitk.ac.in>
2020-05-11 12:58:22 +0530

    read file and show in webpage

--- mysite/views.py
+++ mysite/views.py
@@ -4,4 +4,10 @@
 '''
 from django.http import HttpResponse
 def  index(request):
-    return HttpResponse("yeah! this is first Django site")
+    return HttpResponse("<h1>yeah! this is first Django site</h1>")
+def  about(request):
+    return HttpResponse("This is about Django page")
+def  readtext(request):
+    with open('E:/IIT KANPUR CSE/djangotuts/mysite/mysite/commands.txt') as f:
+        data = f.read()
+        return HttpResponse(data)
