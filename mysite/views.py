b43e43d759c0de320f2f4cbbe36505505a39ad9c
Deepesh Chaudhari <deepeshc@cse.iitk.ac.in>
2020-05-11 15:33:35 +0530

    "html pages added by templates"

--- mysite/views.py
+++ mysite/views.py
@@ -3,11 +3,14 @@
     Deepesh Chaudhari
 '''
 from django.http import HttpResponse
+from  django.shortcuts import render
 def  index(request):
-    return HttpResponse("<h1>yeah! this is first Django site</h1>")
+    params = {'name': 'Deepesh Chaudhari', 'place': 'Kanpur'}
+    return render(request,'index.html',params)
 def  about(request):
-    return HttpResponse("This is about Django page")
+    return render(request,'about.html')
 def  readtext(request):
     with open('E:/IIT KANPUR CSE/djangotuts/mysite/mysite/commands.txt') as f:
         data = f.read()
-        return HttpResponse(data)
+    params = {'data':data}
+    return render(request,'readtext.html',params)
